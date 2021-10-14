ed5d40c4d5fde7bce1296b366089024c5a44c534
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-14 15:21:34 +0630

    [main] increase indent of space to 4 for index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>PHP Basic</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basic</title>
 </head>
 <body>
-   <?php
-      /* Variable */
-      echo "<h2>Variable</h2><br>";
-      $name = "John Doe";
-      echo "Hello, $name<br>";
-   ?>
+    <?php
+        /* Variable */
+        echo "<h2>Variable</h2><br>";
+        $name = "John Doe";
+        echo "Hello, $name<br>";
+    ?>
 </body>
 </html>
