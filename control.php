7476796fc128f6c9bc7680f20af9bd873df64f20
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-14 21:40:58 +0630

    [control] add "If Statement" to control.php

--- control.php
+++ control.php
@@ -6,6 +6,25 @@
     <title>PHP Basic: Control Structures</title>
 </head>
 <body>
+    <?php
+        /* If Statement */
+        echo "<h2>If Statement</h2><br>";
+        $a = 5;
+        $b = 2;
 
+        /* Option 1 */
+        echo ($a == $b)? "T": "F";
+        echo "<br>";
+
+        /* Option 2 */
+        if ($a > $b) {
+            echo "a is greater";
+        } elseif ($a < $b) {
+            echo "b is greater";
+        } else {
+            echo "The Same";
+        }
+        echo "<br>";
+    ?>
 </body>
 </html>
