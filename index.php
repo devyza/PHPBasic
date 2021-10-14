de1cdc2d313b2bb7ccb1f6e8d66380677331d3e6
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-14 15:28:44 +0630

    [main] add "Data Types" to index.php

--- index.php
+++ index.php
@@ -11,6 +11,20 @@
         echo "<h2>Variable</h2><br>";
         $name = "John Doe";
         echo "Hello, $name<br>";
+
+        /* Data Types */
+        echo "<h2>Data Types</h2><br>";
+        $intVar = 5;
+        $doubleVar = 3.14;
+        $stringVar = "Hello World";
+        $boolVar = true;
+        $arrVar = array("Apple", "Banana", "Orange");
+
+        echo $intVar . "<br>";
+        echo $doubleVar . "<br>";
+        echo $stringVar . "<br>";
+        echo $boolVar . "<br>";
+        echo $arrVar[1] . "<br>";
     ?>
 </body>
 </html>
