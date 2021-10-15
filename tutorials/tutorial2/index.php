cdb2e620b5bcd671b342562e3ae7192f9ed264e1
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-15 13:01:42 +0630

    [tutorial2] initial commit to style.css

--- tutorials/tutorial2/index.php
+++ tutorials/tutorial2/index.php
@@ -3,9 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
   <title>Tutorial 2</title>  
 </head>
 <body>
+  <?php
+    $input = 5;
+    for ($row=0; $row < $input; $row++) {
+
+      for ($col = $input+1; $col > $input-$row; $col--) {
+        echo "<span>x</span>";
+      }
+
+      for ($col = 0; $col < (2 * $row + 1); $col++) {
+        echo  "<span>*</span>";
+      }
+      echo "<br>";
+    }
+  ?>
 
 </body>
 </html>
