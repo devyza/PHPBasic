d5b6319b605bff0fb1f9fda45f8ff0aaa276d2eb
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-15 11:36:20 +0630

    [tutorial1] add comments into index.php

--- tutorials/tutorial1/index.php
+++ tutorials/tutorial1/index.php
@@ -12,7 +12,15 @@
             for ($row = 0; $row < 8; $row++) {
                 echo "<tr>";
                     for($col=0; $col < 8; $col++) {
+                        /* 
+                            $row must also be calculate to prevent 
+                            from color reset.
+                            Example:    ($row=1 + $col=2) % 2 = 1
+                                        ($row=0 + $col=8) % 2 = 0
+                        */
                         $class = ($col+$row) % 2 ? "white" : "black";
+
+                        // Assign class name into table column.
                         echo "<td class=$class></td>";
                     }
                 echo "</tr>";
