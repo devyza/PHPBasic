96ecf045bf1084e37f5bc8c73dc77e206c0607d4
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-15 13:47:34 +0630

    [tutorial2]add solution to tutorial2

--- tutorials/tutorial2/index.php
+++ tutorials/tutorial2/index.php
@@ -8,18 +8,32 @@
 </head>
 <body>
   <?php
+  
     $input = 5;
-    for ($row=0; $row < $input; $row++) {
-
-      for ($col = $input+1; $col > $input-$row; $col--) {
-        echo "<span>x</span>";
+    for ($row=0; $row < $input; $row++) {      
+      for ($col = $input; $col-$row-1 > 0; $col--) {
+        echo "<span>&nbsp;</span>";
       }
 
       for ($col = 0; $col < (2 * $row + 1); $col++) {
         echo  "<span>*</span>";
       }
+
       echo "<br>";
     }
+
+    for ($row = 0; $row < $input-1; $row++) {
+      for ($col = 0; $col < $row+1; $col++) {
+        echo "<span>&nbsp;<span>";
+      }
+
+      for ($col = $input; $col - (2* $row - 1) >= 0; $col--) {
+        echo  "<span>*</span>";
+      }
+
+      echo "<br>";
+    }
+
   ?>
 
 </body>
