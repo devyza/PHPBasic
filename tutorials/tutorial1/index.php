6f7adc1699770e8fbaaaf99fde8e08007f7a3c24
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-15 11:27:04 +0630

    [tutorial1] add chess solution

--- tutorials/tutorial1/index.php
+++ tutorials/tutorial1/index.php
@@ -7,6 +7,17 @@
     <title>Tutorial 1</title>
 </head>
 <body>
-    
+    <table class="tbl-chess">
+        <?php
+            for ($row = 0; $row < 8; $row++) {
+                echo "<tr>";
+                    for($col=0; $col < 8; $col++) {
+                        $class = ($col+$row) % 2 ? "white" : "black";
+                        echo "<td class=$class></td>";
+                    }
+                echo "</tr>";
+            }
+        ?>
+    </table>
 </body>
 </html>
