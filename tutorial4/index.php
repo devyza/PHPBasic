96b4b2785fee95df5d7a9f1913e9ac330867f3cb
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-18 12:25:09 +0630

    [tutorial4] add solution to tutorial4

---
+++ tutorial4/index.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    //  Check if the user name is set or not.
+    if (isset($_SESSION['uname']))
+        echo "<script>window.location.href = 'welcome.php'</script>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial 4</title>
+</head>
+<body>
+     <form action="welcome.php" method="post">
+        <label for="username">Username: </label>
+        <input type="text" name="username">
+        <label for="password">Password: </label>
+        <input type="password" name="password">
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
