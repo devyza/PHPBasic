27f944cf8f694c803fc8e525a7ef5f150e5d52e6
Ye Zaw Aung <freeman404.it@gmail.com>
2021-10-21 02:45:41 +0630

    [tutorial6] reformat code

--- tutorial6/upload.php
+++ tutorial6/upload.php
@@ -1,29 +1,29 @@
 <?php
 
-    $ROOT_DIR = 'uploads/';
-    $IMG_SIZE_LIMIT = 5242880;  // 5MB (5 * 1024^2)
+    define('ROOT_DIR', 'uploads/');
+    define('IMG_SIZE_LIMIT', 5242880);  // 5MB (5 * 1024^2)
     
     // Check if there's POST request or not
     if (!isset($_POST['submit'])) {
         return;
     }
     
-    $TARGET_DIR = $ROOT_DIR . $_POST['txtPath'] . "/";
-    $TARGET_FILE = $TARGET_DIR . $_FILES['imgUpload']['name'];
+    $targetDir = ROOT_DIR . $_POST['txtPath'] . "/";
+    $targetFile = $targetDir . $_FILES['imgUpload']['name'];
 
     // Check if root directory is exist
-    if (!is_dir($ROOT_DIR)) {
-        mkdir($ROOT_DIR);
+    if (!is_dir(ROOT_DIR)) {
+        mkdir(ROOT_DIR);
     }
 
     // Check if the image is already uploaded
-    if (file_exists($TARGET_FILE)) {
+    if (file_exists($targetFile)) {
         echo "File Already Exist";
         return;
     }
 
     // Check File Size
-    if ($_FILES['imgUpload']['size'] > $IMG_SIZE_LIMIT) {
+    if ($_FILES['imgUpload']['size'] > IMG_SIZE_LIMIT) {
         echo "File Size is too Large";
         return;
     }
@@ -38,14 +38,13 @@
     }
 
     // Check if the folder is exist.
-    if (!is_dir($TARGET_DIR)) {
-        mkdir($TARGET_DIR);
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir);
     }
     
     // Upload the image file
-    if (move_uploaded_file($_FILES['imgUpload']['tmp_name'], $TARGET_FILE)) {
+    if (move_uploaded_file($_FILES['imgUpload']['tmp_name'], $targetFile)) {
         echo "File Has Been Successfully Uploaded";
-        header('');
     } else {
         echo "Error in Uploading Image File";
     }
